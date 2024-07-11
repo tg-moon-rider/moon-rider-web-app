6d00aa8d240650e155e2794b0cd305965e98feb1
Danil Rodionov <Danil_Rodionov@epam.com>
2024-07-11 23:45:16 +0400

    feat: add additional ton related packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import WebApp from "@twa-dev/sdk";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 WebApp.ready();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+// this manifest is used temporarily for development purposes
+const manifestUrl =
+  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>
+  </TonConnectUIProvider>
 );
